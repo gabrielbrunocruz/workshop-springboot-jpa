7fce016ca04c8424def38d17bb67d2105358aa0d
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-08-17 15:02:22 -0300

    project created

---
+++ src/main/java/com/educandoweb/courseJavaNelio/CourseJavaNelioApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseJavaNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJavaNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJavaNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/courseJavaNelio/CourseJavaNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseJavaNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
