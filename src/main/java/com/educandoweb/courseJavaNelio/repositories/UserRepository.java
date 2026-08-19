63de7ecee871ee29a8fc49169474a502202014ea
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-08-19 15:59:43 -0300

    Service Layer, component registration

--- src/main/java/com/educandoweb/courseJavaNelio/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/courseJavaNelio/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.educandoweb.courseJavaNelio.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educandoweb.courseJavaNelio.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
