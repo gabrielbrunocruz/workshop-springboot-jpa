d29b0cbe194fff5bcba1e740228235ef7fb5731a
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-08-23 21:12:09 -0300

    Product

---
+++ src/main/java/com/educandoweb/courseJavaNelio/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.courseJavaNelio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.courseJavaNelio.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
