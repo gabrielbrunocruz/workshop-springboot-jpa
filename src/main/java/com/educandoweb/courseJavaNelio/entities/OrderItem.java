70abfc4f1ee75a222abc6ebf9420b43941538ff7
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-09-08 23:21:45 -0300

    Subtotal and Total methods

--- src/main/java/com/educandoweb/courseJavaNelio/entities/OrderItem.java
+++ src/main/java/com/educandoweb/courseJavaNelio/entities/OrderItem.java
@@ -65,6 +65,11 @@ public class OrderItem implements Serializable {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	
+	public Double getSubTotal() {
+		return price*quantity;
+	}
 
 	@Override
 	public int hashCode() {
@@ -84,5 +89,4 @@ public class OrderItem implements Serializable {
 	}
 	
 	
-
 }
