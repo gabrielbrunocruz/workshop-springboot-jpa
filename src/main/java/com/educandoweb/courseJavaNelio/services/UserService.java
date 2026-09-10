e2c166799ed522c21670ae21257a6be82094d3ee
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-09-10 18:12:04 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/courseJavaNelio/services/UserService.java
+++ src/main/java/com/educandoweb/courseJavaNelio/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.courseJavaNelio.entities.User;
 import com.educandoweb.courseJavaNelio.repositories.UserRepository;
+import com.educandoweb.courseJavaNelio.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
